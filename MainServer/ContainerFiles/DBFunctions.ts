5644e33fa1ccff99ea9353c0d56cb6279a8c9cc3
Artimus100 <rahulmishrapu22@gmail.com>
2025-07-13 22:05:46 +0530

    updated dbfuncs and indexer

--- MainServer/ContainerFiles/DBFunctions.ts
+++ MainServer/ContainerFiles/DBFunctions.ts
@@ -1,55 +1,43 @@
-import { MongoClient, ObjectId } from 'mongodb'
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 
+// loader: Get item from table by primary key
+export async function loader(entityName: string, primId: string, primName: string): Promise<any | undefined> {
+  try {
+    // @ts-ignore - dynamic access
+    const data = await prisma[entityName].findUnique({
+      where: {
+        [primName]: primId,
+      },
+    });
 
-const url = process.env.MONGO_URL == null ? 'mongodb://localhost:27017' : process.env.MONGO_URL;
-const client = new MongoClient(url);
-const db = client.db('Indexer');
-console.log(url);
-
-
-export async function loader(entityName : string,primId : string, primName : string) : Promise<undefined|object> {
-    try {
-        await client.connect()
-    let cols = await db.collections();
-    if (cols.find(x => process.env.PROJECT_ID + "." + entityName === x.collectionName) === undefined) {
-        console.log("collection not found");
-        return undefined;
-    } 
-    let col = db.collection(process.env.PROJECT_ID + "." + entityName);
-    let data = await col.find({[primName] : primId}).toArray();
-    if (data.length == 0) {
-        console.log("data not found");
-        return undefined;
-    }
-    return data[0];
-    } catch (error) {
-        console.log(error);
-        console.log("err in loader");
-        
-        
+    if (!data) {
+      console.log(`[Prisma] No data found in ${entityName} where ${primName} = ${primId}`);
+      return undefined;
     }
 
-    
+    return data;
+  } catch (err) {
+    console.error(`[Prisma] Loader error: ${err}`);
+    return undefined;
+  }
 }
 
-export async function saver(entityName : string,item : object, primId : string, primName : string) {
-    try {
-        await client.connect()
-    let col = db.collection(process.env.PROJECT_ID + "." + entityName);
-    console.log({[primName] : primId});
-    let data = await col.find({[primName] : primId}).toArray();
-    if (data.length == 0) {
-        console.log("inserting");
-        await col.insertOne(item);
-    } 
-    else {
-        console.log("updating");
-        await col.updateOne({[primName] : primId},{$set : item});
-    }
-    } catch (error) {
-        console.log(error);
-        console.log("err in saver");
-    }
-    
+// saver: Upsert (insert if not found, else update)
+export async function saver(entityName: string, item: any, primId: string, primName: string): Promise<void> {
+  try {
+    // @ts-ignore - dynamic model access
+    await prisma[entityName].upsert({
+      where: {
+        [primName]: primId,
+      },
+      update: item,
+      create: item,
+    });
+
+    console.log(`[Prisma] Saved to ${entityName} where ${primName} = ${primId}`);
+  } catch (err) {
+    console.error(`[Prisma] Saver error: ${err}`);
+  }
 }
